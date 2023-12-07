f55c67438dafbd813fabf152a9d08cd7cd2b99a5
axeldealex <axeldelex@gmail.com>
2023-12-07 20:56:04 +0100

    finished day 6

--- day6/boat_speed.py
+++ day6/boat_speed.py
@@ -1,4 +1,6 @@
-with open('day6/test_input.txt', 'r') as f:
+import math
+
+with open('day6/final_input_again.txt', 'r') as f:
     times = f.readline()
     dist = f.readline()
 
@@ -7,8 +9,23 @@ dist = dist.split(':')[1].split()
 
 time_dist = dict()
 for i, time in enumerate(times):
-    time_dist[int(time)] = int(dist[i])
+    time_dist[i] = [int(time), int(dist[i])]
 
+print(time_dist)
+product = 1
+# goes over all the times
+for entry in time_dist.keys():
+    # starts finding the minimum gas time to win, max is symmetrical to the other side
+    target = time_dist[entry][1]
+    time = time_dist[entry][0]
+    print(f'need to hit {target} in {time}')
+    for i in range(1, math.ceil(time + 1)):
+        distance = (time - i) * i
+        if distance > target:
+            print(f'got to {distance} compared to {target}')
+            min_time = i
+            max_time = time - i
+            product *= (max_time - min_time + 1)
+            break
 
-# now need to determine how long to PRESS THE GAS!
-# start in the middle, then work to the lowest time and highest time to press the gas and win
+print(product)
